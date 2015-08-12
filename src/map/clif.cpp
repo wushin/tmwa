9e3177fed0e2ea1f7df56bace3862865b21fccad
mekolat <mekolat@users.noreply.github.com>
2015-08-12 14:53:24 -0400

    do not send spell invocation back to the caster

--- src/map/clif.cpp
+++ src/map/clif.cpp
@@ -3822,10 +3822,10 @@ RecvResult clif_parse_GlobalMessage(Session *s, dumb_ptr<map_session_data> sd)
         Buffer sendbuf = create_vpacket<0x008d, 8, 1>(head_8d, repeat_8d);
 
         clif_send(sendbuf, sd, SendWho::AREA_CHAT_WOC, MIN_CLIENT_VERSION);
-    }
 
-    /* Send the message back to the speaker. */
-    send_packet_repeatonly<0x008e, 4, 1>(s, repeat);
+        /* Send the message back to the speaker. */
+        send_packet_repeatonly<0x008e, 4, 1>(s, repeat);
+    }
 
     return rv;
 }
